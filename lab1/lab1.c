7641f4df811fb849545a17b36f325de84393d5c5
Manjari Prasad <mps12@sfu.ca>
2025-09-12 18:04:42 -0700

    Modified lab1.c to tokenize input using strtok_r

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
   char *line = NULL; // getline would allocate this buffer
@@ -16,7 +17,16 @@ int main(void) {
     return 1;
   }
 
-  printf("You have typed (%zd chars): %s", nread, line);
+  puts("Tokens:");
+
+  // split on spaces tabs and newlines
+
+  char *save = NULL;
+  char *tok = strtok_r(line, " \t\n", &save);
+  while (tok) {
+    printf(" %s\n", tok);
+    tok = strtok_r(NULL, " \t\n", &save);
+  }
 
   free(line); // to cleanupp
   return 0;
